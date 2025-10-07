1508392efead59133e18851e41c2c49a903e40e9
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2025-10-07 11:28:11 -0300

    Change focus to IDR studies

--- assets/js/example-queries.js
+++ assets/js/example-queries.js
@@ -1,69 +1,56 @@
 var example_1 = {
   "distinct": true,
   "variables": [
-    "Museum_1"
-  ],
-  "order": null,
-  "branches": [
     {
-      "line": {
-        "s": "?Museum_1",
-        "p": "http://ontologies.sparna.fr/sparnatural-demo-dbpedia#country",
-        "o": "?Country_2",
-        "sType": "http://ontologies.sparna.fr/sparnatural-demo-dbpedia#Museum",
-        "oType": "http://ontologies.sparna.fr/sparnatural-demo-dbpedia#Country",
-        "values": [
-          {
-            "label": "France (2)",
-            "rdfTerm": {
-              "type": "uri",
-              "value": "http://dbpedia.org/resource/France"
-            }
-          }
-        ]
-      },
-      "children": []
+      "termType": "Variable",
+      "value": "Study_1"
     }
-  ]
-}
-
-var example_2 = {
-  "distinct": true,
-  "variables": [
-    "Artwork_1"
   ],
   "order": null,
   "branches": [
     {
       "line": {
-        "s": "?Artwork_1",
-        "p": "http://ontologies.sparna.fr/sparnatural-demo-dbpedia#author",
-        "o": "?Person_2",
-        "sType": "http://ontologies.sparna.fr/sparnatural-demo-dbpedia#Artwork",
-        "oType": "http://ontologies.sparna.fr/sparnatural-demo-dbpedia#Person",
+        "s": "Study_1",
+        "p": "https://data.example.com/ontologies/sparnatural-config/organism",
+        "o": "OrganismBinomial_2",
+        "sType": "https://data.example.com/ontologies/sparnatural-config/Study",
+        "oType": "https://data.example.com/ontologies/sparnatural-config/OrganismBinomial",
         "values": []
       },
       "children": [
         {
           "line": {
-            "s": "?Person_2",
-            "p": "http://ontologies.sparna.fr/sparnatural-demo-dbpedia#movement",
-            "o": "?Movement_4",
-            "sType": "http://ontologies.sparna.fr/sparnatural-demo-dbpedia#Person",
-            "oType": "http://ontologies.sparna.fr/sparnatural-demo-dbpedia#Movement",
-            "values": [
-              {
-                "label": "Impressionism (232)",
-                "rdfTerm": {
-                  "type": "uri",
-                  "value": "http://dbpedia.org/resource/Impressionism"
-                }
-              }
-            ]
+            "s": "OrganismBinomial_2",
+            "p": "https://data.example.com/ontologies/sparnatural-config/Wikidata_has_organism_name",
+            "o": "WikidataTaxonID_4",
+            "sType": "https://data.example.com/ontologies/sparnatural-config/OrganismBinomial",
+            "oType": "https://data.example.com/ontologies/sparnatural-config/WikidataTaxonID",
+            "values": []
           },
-          "children": []
+          "children": [
+            {
+              "line": {
+                "s": "WikidataTaxonID_4",
+                "p": "https://data.example.com/ontologies/sparnatural-config/organismParentTaxon",
+                "o": "WikidataTaxonID_6",
+                "sType": "https://data.example.com/ontologies/sparnatural-config/WikidataTaxonID",
+                "oType": "https://data.example.com/ontologies/sparnatural-config/WikidataTaxonID",
+                "values": [
+                  {
+                    "label": "Plantae",
+                    "rdfTerm": {
+                      "type": "uri",
+                      "value": "http://www.wikidata.org/entity/Q756"
+                    }
+                  }
+                ]
+              },
+              "children": []
+            }
+          ]
         }
       ]
     }
-  ]
+  ],
+  "limit": 1000
 }
